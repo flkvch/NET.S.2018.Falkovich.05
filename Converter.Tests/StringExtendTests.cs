02477d15b0a40d84b85d40dd8e91e171ac1380bd
Ksenia <ksenya-f3@rambler.ru>
2018-07-07 15:30:30 +0300

    The First Commit

---
+++ Converter.Tests/StringExtendTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Converter.Tests
+{
+    public static class StringExtendTests
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = 62370570)]
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        [TestCase("10", 5, ExpectedResult = 5)]
+        [TestCase("100", 10, ExpectedResult = 100)]
+        [TestCase("0", 6, ExpectedResult = 0)]
+        [TestCase("11111111111111111111111111111111", 2, ExpectedResult = uint.MaxValue)]
+        [TestCase("32244002423140", 5, ExpectedResult = uint.MaxValue)]
+        [TestCase("37777777777", 8, ExpectedResult = uint.MaxValue)]
+        public static uint StringToUInt32_ValidCases(this string number, int radix)
+    => StringExtend.StringToUInt32(number, radix);
+
+        [TestCase("1AeF101", 2)]
+        [TestCase("764241", 20)]
+        [TestCase("SA123", 2)]
+        public static void StringToUint32_ArgumentExeption(this string number, int radix)
+    => Assert.Throws<ArgumentException>(() => StringExtend.StringToUInt32(number, radix));
+
+        [TestCase("100000000000000000000000000000000", 2)]
+        [TestCase("102002022201221111211", 3)]
+        [TestCase("10000000000000000", 4)]
+        [TestCase("111111111111111111111111111111111111111111111", 2)]
+        [TestCase("100000000", 16)]
+        public static void StringToUint32_OverFlowExeption(this string number, int radix)
+    => Assert.Throws<OverflowException>(() => StringExtend.StringToUInt32(number, radix));
+    }  
+}
---
+++ Converter/StringExtend.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Converter
+{
+    /// <summary>
+    /// Extentions of the <see cref="System.String"/>
+    /// </summary>
+    public static class StringExtend
+    {
+        /// <summary>
+        /// Converts integer positive number from its string representation to <see cref="UInt32"/>.
+        /// </summary>
+        /// <param name="numberString">String representation of number.</param>
+        /// <param name="radix">The radix of the number.</param>
+        /// <returns>The result of converting.</returns>
+        /// <exception cref="ArgumentException">radix</exception>
+        /// <exception cref="OverflowException">Number is bigger than UInt.MaxValue.</exception>
+        public static uint StringToUInt32(this string numberString, int radix)
+        {
+            long result = 0;
+            if (numberString == "0")
+            {
+                return 0;
+            }
+
+            Notation notation = new Notation(radix);
+            numberString = numberString.ToUpper();
+            int length = numberString.Length;
+            for (int i = 0; i < length; i++)
+            {              
+                int numberOfSymbol = notation.Symbols.IndexOf(numberString[i]);
+                if (numberOfSymbol == -1)
+                {
+                    throw new ArgumentException(nameof(radix));
+                }
+
+                
+                result += numberOfSymbol * (long)Math.Pow(notation.Radix, length - 1 - i);
+            }
+
+            if (result > uint.MaxValue)
+            {
+                throw new OverflowException("Number is bigger than UInt.MaxValue.");
+            }
+
+            return (uint)result;
+        }
+
+        private struct Notation
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Notation"/> struct.
+            /// </summary>
+            /// <param name="radix">
+            /// The radix.
+            /// </param>
+            /// <exception cref="ArgumentException">Radix was less than 2 or bigger than 16.</exception>
+            public Notation(int radix)
+            {
+                if (radix < 2 || radix > 16)
+                {
+                    throw new ArgumentException("Radix was less than 2 or bigger than 16.", nameof(radix));
+                }
+
+                Radix = radix;
+                Symbols = Alphabet.Substring(0, radix);
+            }
+
+            /// <summary>
+            /// Gets the alphabet.
+            /// </summary>
+            /// <value>
+            /// The alphabet.
+            /// </value>
+            private static string Alphabet { get; } = "0123456789ABCDEF";
+
+            /// <summary>
+            /// Gets radix.
+            /// </summary>
+            /// <value>
+            /// Gets radix.
+            /// </value>
+            public int Radix { get; }
+
+            /// <summary>
+            /// Gets symbols of the notation.
+            /// </summary>
+            /// <value>
+            /// Gets symbols of the notation.
+            /// </value>
+            public string Symbols { get; }
+        }
+    }    
+}
